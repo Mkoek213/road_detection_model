5386984d34e17a16a25fb5839a43365dd713b585
Cz4rNy213 <mikolaj.kolek2@gmail.com>
2024-07-24 16:24:06 +0200

    tests

---
+++ road_detection_model/live.py
@@ -0,0 +1,41 @@
+from ultralytics import YOLO
+import cv2
+import cvzone
+import math
+
+cap = cv2.VideoCapture(0)
+cap.set(3, 640)
+cap.set(4, 480)
+
+model = YOLO("Models/yolov8n.pt")
+
+classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "potted plant", "bed", "dining table",  "toilet", "tv monitor", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
+
+
+
+while True:
+    success, img = cap.read()
+    results = model(img, stream = True)
+    for r in results:
+        boxes = r.boxes
+        for box in boxes:
+
+            # bounding box
+            x1, y1, x2, y2 = box.xyxy[0]
+            # x1, y1, w, h = box.xywh[0]
+            x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
+            w, h = x2 - x1, y2 - y1
+            bbox = int(x1), int(y1), int(w), int(h)
+            # print(x1, y1, x2, y2)
+            # cv2.rectangle(img, (x1, y1), (x2, y2), (255, 200 ,255), 3)
+            cvzone.cornerRect(img, bbox)
+
+            # confidence
+            conf = math.floor(box.conf[0]* 100)/100
+
+            # class name
+            cls = box.cls[0]
+            cvzone.putTextRect(img, f"{classNames[int(cls)]} {conf}", (max(0, x1), max(35, y1)))
+
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
--- road_detection_model/main.py
+++ road_detection_model/main.py
@@ -1,1 +1,6 @@
-from ultralytics import yolo
+from ultralytics import YOLO
+import cv2
+
+model = YOLO('Models/yolov8l.pt')
+results = model("Images/2.jpg", show = True)
+cv2.waitKey(0)
