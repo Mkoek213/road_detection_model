e4f7679a69e0ac9daf623d2486c2fe6a86bd4c7c
Mkoek213 <mikolaj.kolek2@gmail.com>
2024-07-25 10:38:01 +0200

    Cuda completed, both py files and notebooks works on GPU

--- road_detection_model/live.py
+++ road_detection_model/live.py
@@ -8,13 +8,11 @@ import math
 # cap.set(4, 480)
 cap = cv2.VideoCapture("Images/video1.webm") # for video
 
-
-model = YOLO("Models/yolov8n.pt")
+model = YOLO("Models/yolov8l.pt")
 
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "potted plant", "bed", "dining table",  "toilet", "tv monitor", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
 
-
-
+print(cv2.cuda.getCudaEnabledDeviceCount())
 while True:
     success, img = cap.read()
     results = model(img, stream = True)
@@ -40,4 +38,4 @@ while True:
             cvzone.putTextRect(img, f"{classNames[int(cls)]} {conf}", (max(0, x1), max(35, y1)))
 
     cv2.imshow("Image", img)
-    cv2.waitKey(5)
+    cv2.waitKey(1)
