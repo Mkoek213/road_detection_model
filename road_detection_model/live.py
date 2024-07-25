c39470626471216aef5554551e8cc09332da1c2c
Mkoek213 <mikolaj.kolek2@gmail.com>
2024-07-25 20:05:20 +0200

    code for converting COCO format to YOLO format done, YOLO format: <class_id> <x_center> <y_center> <width> <height>

--- road_detection_model/live.py
+++ road_detection_model/live.py
@@ -8,11 +8,10 @@ import math
 # cap.set(4, 480)
 cap = cv2.VideoCapture("Images/video1.webm") # for video
 
-model = YOLO("Models/yolov8l.pt")
+model = YOLO("Models/yolov8n.pt")
 
 classNames = ["person", "bicycle", "car", "motorbike", "aeroplane", "bus", "train", "truck", "boat", "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat", "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack", "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball", "kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket", "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple", "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair", "sofa", "potted plant", "bed", "dining table",  "toilet", "tv monitor", "laptop", "mouse", "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator", "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"]
 
-print(cv2.cuda.getCudaEnabledDeviceCount())
 while True:
     success, img = cap.read()
     results = model(img, stream = True)
